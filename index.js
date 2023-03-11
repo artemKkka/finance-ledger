254bbea46e185227bf325767570293268263dafd
artemKkka <93820322+artemKkka@users.noreply.github.com>
2023-03-11 12:42:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    fixedHeader()
+    $(window).scroll(function () {
+        fixedHeader()
+    });
+    function fixedHeader() {
+        if (window.pageYOffset > 0) {
+            $(".header").addClass("fixed")
+        } else {
+            $(".header").removeClass("fixed")
+        }
+    }
+    $(".burger, .overlay, .header__menu a").click(function () {
+        toggleMenu()
+    });
+    function toggleMenu() {
+        if ($(window).width() < 1024) {
+            $(".header").toggleClass("active")
+            $("body").toggleClass("menu-is-opened")
+        }
+    }
+});
+
+function func(str, a, b) {
+    if (str === 'sum') {
+        return a + b
+    } else if (str === multiply) {
+        return a * b
+    } else{
+        return 'Такої опеації не існує'
+    }
+}
+
+console.log(func("sum", 4, 5))
